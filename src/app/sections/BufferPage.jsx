a3e46af0ca55ecd22ba0f93bd19614c5347da9e2
ikkai-r <rica_sales@dlsu.edu.ph>
2025-05-07 09:58:19 +0800

    style: changed spacing for bufferpage

--- src/app/sections/BufferPage.jsx
+++ src/app/sections/BufferPage.jsx
@@ -1,10 +1,10 @@
 
 export default function BufferPage() {
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <p>Please perform the other task before proceeding.</p>
+      <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <p className="text-xl">Please perform the other task before proceeding.</p>
 
-          <p>Click the next button below to start the assessments.</p>
+          <p className="text-xl">Click the next button below to start the assessments.</p>
       </div>
     );
   }

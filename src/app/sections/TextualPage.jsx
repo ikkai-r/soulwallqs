c6b0133436600240b616c0a2ab7008404f1d8836
ikkai-r <rica_sales@dlsu.edu.ph>
2025-05-07 15:24:05 +0800

    feat: finished textual page

--- src/app/sections/TextualPage.jsx
+++ src/app/sections/TextualPage.jsx
@@ -1,8 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
 
 export default function TextualPage() {
+
+  const imageList = [
+    '1.png', '2.png', '3.png', '4.png', '5.png',
+    '6.png', '7.png', '8.png', '9.png', '10.png',
+    '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg',
+    '16.jpg', '17.jpg', '18.png', '19.jpg', '20.jpg'
+  ];
+
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          this is textual
+      <div className="flex flex-col items-center min-h-screen p-10 gap-12 font-[family-name:var(--font-geist-sans)] w-full">
+           <form className='grid grid-cols-4 gap-32 w-full'>
+            
+           {imageList.map((filename, index) => (
+              <div key={index} className="relative z-0 w-full mb-5 group">
+                <Image
+                  src={`/img/${filename}`}
+                  alt={`Image ${index + 1}`}
+                  width={300}
+                  height={300}
+                  className="object-contain"
+                />
+                <input
+                  type="text"
+                  name={`art${index + 1}`}
+                  id={`art${index + 1}`}
+                  className="block py-2.5 px-0 w-full mt-2 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-900 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600"
+                  placeholder=" "
+                  required
+                />
+              </div>
+            ))}
+
+          </form>
       </div>
     );
   }
